782f098f3c2a1a5eb3072c1aef350b61489a8edb
Taylor Daugherty <taylor.l.daugherty@gmail.com>
2016-01-21 17:05:17 -0600

    Solve challenge

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -4,6 +4,7 @@
 
 
 # Your Solution Below
+
 def factorial(num)
   if num > 0
   #until num == 1
@@ -12,3 +13,19 @@ def factorial(num)
     return 1
   end
 end
+
+
+=begin
+  num2 = num.to_i - 1
+  num2 -= 1
+  if num > 0
+    return num * num2
+  elsif num == 0
+    return 1
+  else
+    return nil
+  end
+end
+=end
+
+
