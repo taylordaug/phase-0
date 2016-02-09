6ed4e37b2173c54595a2f3ce3cb939af134132f6
Taylor Daugherty <taylor.l.daugherty@gmail.com>
2016-02-09 15:06:00 -0600

    Practice js challenges

---
+++ week-7/eloquent.js
@@ -0,0 +1,27 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node eloquent.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+var taylor = 25
+console.log(taylor + 2)
+
+//Prompt program
+prompt("What is your favorite food?");
+alert("Hey! That's my favorite too!");
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+//Looping a Triangle:
+
+for (var string = "#"; string.length < 8; string += "#")
+  console.log(string);
+
+// Functions
+// Complete the `minimum` exercise.
+function min(x, y) {if (x < y) return x; else return y;}
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+var me = ["Taylor", 25, "sushi", "mac and cheese", "ramen", "I received a red belt in tae kwon do when I was 10"];
