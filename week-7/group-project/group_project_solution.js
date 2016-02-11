80401b66b599b2340b42271bbada4aab29204e3a
Taylor Daugherty <taylor.l.daugherty@gmail.com>
2016-02-10 16:51:16 -0800

    Add person 1 solution

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,6 @@
+// User Stories - Taylor
+// As a user, I want to use a function "sum" to find the sum of all elements in an array. This needs to work for both an array of odd length and an array of even length.
+
+// As a user, I want to use a function "mean" to find the average of all elements in an array. This needs to work for both an array of odd length and an array of even length.
+
+// As a user, I want to use a function "median" to find the median of all elements in an array. This needs to work for both an array of odd length and an array of even length.
