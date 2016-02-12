88319324222e63e1c112489c0a28308c9d2a9925
Taylor Daugherty <taylor.l.daugherty@gmail.com>
2016-02-12 12:48:32 -0800

    Add reflection and olympics solutions

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -2,18 +2,20 @@
 
 // I paired with Ivy Vetor on this challenge.
 
-// This challenge took me [#] hours.
-
-
-// Warm Up
-
-
-
+// This challenge took me 2 hours.
 
 // Bulk Up
+var athletes = [
+  {name: "Sarah Hughes", event: "Ladies' Singles"}, {name: "Bruce Jenner", event: "Men's Triathalon"}]
 
 
+var win = function(athletes){
+  for(var counter = 0; counter < athletes.length; counter ++){
+    athletes[counter].win = console.log((athletes[counter].name) + " won the " +       (athletes [counter].event)+ "!")
+  }
+}
 
+win(athletes)
 
 // Jumble your words
 var reverse = function(var1){
@@ -21,8 +23,6 @@ var reverse = function(var1){
 };
 
 reverse("Hello")
-
-
 
 // 2,4,6,8
 var evenArray = []
@@ -35,7 +35,6 @@ var evenFunction = function(array) {
   console.log(evenArray);
 };
 evenFunction([2, 3, 4, 5, 6, 7, 46, 50])
-
 
 
 // "We built this city"
@@ -52,6 +51,10 @@ console.log(michaelPhelps.constructor === Athlete)
 console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
 
 
-
-
 // Reflection
+// What JavaScript knowledge did you solidify in this challenge?
+// I am getting more comfortable with for loops to iterate over objects.
+// What are constructor functions?
+// Constructor functions are like Ruby classes. You can "initialize" in a sense a variety of variables that each new object created from a constructor will take on. You can create a new object using a constructor function that will take on a series of arguments and spit out a new object.
+// How are constructors different from Ruby classes (in your research)?
+// They are very similar, but instead of using instance variables to initialize arguments, you use the keyword "this". Anything with "this" will be a property or method created for the new object.
