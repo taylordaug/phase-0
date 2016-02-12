b69fe41c6a1a101f7683535acf4a3ef7a9a063e2
Taylor Daugherty <taylor.l.daugherty@gmail.com>
2016-02-12 11:56:06 -0800

    Final group solution

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,6 +1,33 @@
-// User Stories - Taylor
-// As a user, I want to use a function "sum" to find the sum of all elements in an array. This needs to work for both an array of odd length and an array of even length.
+// Release 5: Putting it all together
+// Tests passed: sum and mean defined as functions; all tests for median
+// Tests failed: sum and mean for even & odd lengths
 
-// As a user, I want to use a function "mean" to find the average of all elements in an array. This needs to work for both an array of odd length and an array of even length.
+// It looks like the sum and mean functions are broken. While running the code with examples and printing using console.log, I saw that it was printing nothing, so I figured out we actually weren't returning anything. I noticed that for both functions, there was an "i" counter for the index that was not defined with var and wasn't told to count up, it was simply called in function(i) with no context. This could have been broken in the refactor from the for loop to the forEach() method. In trying to fix the forEach syntax, I realized how much more sense the for loop made, so changed the code to iterate that way instead. After this change, all tests passed! The correct solutions for each are below:
 
-// As a user, I want to use a function "median" to find the median of all elements in an array. This needs to work for both an array of odd length and an array of even length.
+// sum
+function sum(array) {
+  var totalSum = 0
+  for(var counter = 0; counter < array.length; counter ++)
+    {totalSum += array[counter];
+  }
+  return(totalSum);
+}
+
+//mean
+function mean(array) {
+  var totalSum = 0;
+  for(var counter = 0; counter < array.length; counter ++)
+    {totalSum += array[counter];
+  }
+  return(totalSum / array.length)
+};
+
+
+//median
+function median(array) {
+  if (array.length % 2 === 0) {
+    return (array[array.length / 2] + array[(array.length / 2) - 1]).toFixed(2) / 2;
+  } else {
+    return array[Math.floor((array.length.toFixed(1) / 2))];
+  }
+};
